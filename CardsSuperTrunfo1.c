964e4bfe521a06073b12394abc2fee7261805859
Joao-sora <jv24112004@gmail.com>
2025-02-02 22:22:42 +0000

    *printf("Ponto Turistico: %d", pontoturistico);

---
+++ CardsSuperTrunfo1.c
@@ -0,0 +1,233 @@
+#include <stdio.h>
+
+int main (){
+    int pontoturistico;
+    float area, pib, populacao;
+    char cidade[60], pais[60], estado[60];
+
+        printf("Carta 1A01\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);
+    
+    printf("Carta 1A02\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);
+
+    printf("Carta 1A03\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);    
+
+    printf("Carta 1A04\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);
+
+    printf("Carta 1B01\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);
+            
+    printf("Carta 1B02\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);                
+
+    printf("Carta 1B03\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);
+
+    printf("Carta 1B04\n");
+        
+        printf("Pais: \n");
+        scanf("%s", &pais);
+
+        printf("Estado: \n");
+        scanf("%s", &estado);
+
+        printf("Cidade: \n");
+        scanf("%s", &cidade);
+
+        printf("Populacao: \n");
+        scanf("%f", &populacao);
+
+        printf("Area: \n");
+        scanf("%f", &area);
+
+        printf("PIB: \n");
+        scanf("%f", &pib);
+
+        printf("Pontos Turisticos: \n");
+        scanf("%d", &pontoturistico);
+
+            printf("Pais: %s - Cidade: %s - Estado: %s \n", pais, cidade, estado);
+            printf("Area: %f - PIB: %f - População: %f \n", area, pib, populacao);
+            printf("Ponto Turistico: %d", pontoturistico);
+
+
+
+
+
+
+
+
+
+return 0;
+}
